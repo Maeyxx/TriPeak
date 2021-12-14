f1f16f734fec9f4ce069bffff812a4cc1e51540e
Gabriel GIBON <gabriel.gibon1@gmail.com>
2021-12-14 15:32:07 +0100

    Update-1 AffectCard

--- AffectCard/affectcard.c
+++ AffectCard/affectcard.c
@@ -2,134 +2,58 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "InitializeCard.h"
+#include <time.h>
 
-Card arraycard[51];
+int i;
 
-int InitializeCard()
-/*
-Initialise le paquet de carte et incrémente chaque structure de carte dans le tableau arraycard.
-*/
-{
-	int specific_card = 0;
-	char symbol = '0';
-	for (int i = 1; i <= 4; i++)
-	{
-		switch (i)
-		{
-		case 1:
-			symbol = 'c';
-			break;
-		case 2:
-			symbol = 'd';
-			break;
-		case 3:
-			symbol = 'h';
-			break;
-		case 4:
-			symbol = 's';
-			break;
+void affectcard(Card arraylayer[], int y, pickcard) {
+	int j = 0;
+	// Affectation des cartes a un layer
+	for (i = 0; i <= y; i++) {
+		pickcard = rand() % 52;
+		if (arraycard[pickcard].value != 0) {
+			arraylayer[i-j] = arraycard[pickcard];
+			arraycard[pickcard].value = 0;
 		}
-		for (int a = 1; a <= 13; a++)
-		{
-			struct card c;
-			c.value = a;
-			c.symbol = symbol;
-			arraycard[specific_card] = c;
-			specific_card += 1;
+		else {
+			j++;
+			y++;
 		}
 	}
-	return 0;
 }
 
-
-
-// Tableau couche 1 à 4
-Card arraylayer1[3];
-Card arraylayer2[6];
-Card arraylayer3[9];
-Card arraylayer4[10];
-
-// Tableau pioche
-Card arraydeck[24];
-
-
-void affectcard(arraycard) {
-	int x;
-	int i;
-	int pickcard;
-
-	// Affectation des cartes a la couche 1
-	x = 3;
-	for (i = 0; i <= x; i++) {
-		pickcard = rand() % len(arraycard);
-		if (arraycard[&pickcard] != NULL) {
-			arraylayer1[i] = arraycard[&pickcard];
-			arraycard[&pickcard] = NULL;
-		}
-		else {
-			x++;
-		}
-	}
-
-	// Affectation des cartes a la couche 2
-	x = 6;
-	for (i = 0; i <= x; i++) {
-		pickcard = rand() % len(arraycard);
-		if (arraycard[&pickcard] != NULL) {
-			arraylayer2[i] = arraycard[&pickcard];
-			arraycard[&pickcard] = NULL;
-		}
-		else {
-			x++;
-		}
-	}
-
-	// Affectation des cartes a la couche 3
-	x = 9;
-	for (i = 0; i <= x; i++) {
-		pickcard = rand() % len(arraycard);
-		if (arraycard[&pickcard] != NULL) {
-			arraylayer3[i] = arraycard[&pickcard];
-			arraycard[&pickcard] = NULL;
-		}
-		else {
-			x++;
-		}
-	}
-
-	// Affectation des cartes a la couche 4
-	x = 10;
-	for (i = 0; i <= x; i++) {
-		pickcard = rand() % len(arraycard);
-		if (arraycard[&pickcard] != NULL) {
-			arraylayer4[i] = arraycard[&pickcard];
-			arraycard[&pickcard] = NULL;
-		}
-		else {
-			x++;
-		}
-	}
-
+void affectcarddeck (Card arraydeck[], pickcard){
+	int h = 0;
+	int g = 23;
 	// Affectation des cartes a la pioche
-	x = len(arraycard);
-	for (i = 0; i <= x; i++) {
-		pickcard = rand() % len(arraycard);
-		if (arraycard[&pickcard] != NULL) {
-			arraydeck[i] = arraycard[&pickcard];
+	for (i = 0; i <= g; i++) {
+		pickcard = rand() % 52;
+		if (arraycard[pickcard].value != 0) {
+			arraydeck[i-h] = arraycard[pickcard];
+			arraycard[pickcard].value = 0;
 		}
 		else {
-			x++;
+			h++;
+			g++;
 		}
 	}
-	return arraylayer1, arraylayer2, arraylayer3, arraylayer4, arraydeck;
 }
-
-
 
 int main(int argc, char* argv[]) {
+	int pickcard;
+	// Tableau couche 1 à 4
+	Card arraylayer1[3];
+	Card arraylayer2[6];
+	Card arraylayer3[9];
+	Card arraylayer4[10];
+	// Tableau pioche
+	Card arraydeck[24];
 	srand(time(NULL));
 	InitializeCard();
-	affectcard(arraycard);
+	affectcard(arraylayer1, 2, pickcard);
+	affectcard(arraylayer2, 5, pickcard);
+	affectcard(arraylayer3, 8, pickcard);
+	affectcard(arraylayer4, 9, pickcard);
+	affectcarddeck(arraycard, pickcard);
 	return 0;
 }
