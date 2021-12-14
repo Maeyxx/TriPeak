c54bd9c7f3cc6ac8c08f1678650b6fa89864bb8e
Alexis Pasquier <Alexispasquier1@gmail.com>
2021-12-14 11:29:52 +0100

    First push

---
+++ InitializeCard/InitializeCard.h
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct card
+{
+  char symbol;
+  // c = Clubs | h = Hearts | d = Diamonds | s = Spades
+  int value;
+  // From 1 to 13
+};
+typedef struct card Card;
+
+
